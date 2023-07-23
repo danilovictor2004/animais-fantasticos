c45cb78d9c194ae4fc935fd680513a748f7512e3
danillo.caetano <danillo.caetano@unimedmaceio.com.br>
2023-07-22 22:14:31 -0300

    Tooltip transformado em classe

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ import Accordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
-
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -23,8 +22,10 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initAnimacaoScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
