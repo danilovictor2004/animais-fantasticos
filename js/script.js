0d859cee7c95bca264cce5db05e93935d8481589
danillo.caetano <danillo.caetano@unimedmaceio.com.br>
2023-07-27 15:47:16 -0300

    correção de bug

--- js/script.js
+++ js/script.js
@@ -37,5 +37,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]');
 funcionamento.init();
 
-fetchAnimais('animais-fantasticos/animaisapi.json', '.numeros-grid');
+fetchAnimais('../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
